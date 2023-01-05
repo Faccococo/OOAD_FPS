d1ecb7657b40f925bd0edd2af75f7dbcf924d199
11910503 <64644882+11910503@users.noreply.github.com>
2023-01-05 13:45:11 +0800

    enemyhit

--- Assets/Codes/AI_control/AI.cs
+++ Assets/Codes/AI_control/AI.cs
@@ -6,9 +6,9 @@ using UnityEngine.AI;
 public class AI : MonoBehaviour
 {
     [Header("本体属性设置")]
-    [SerializeField] [Tooltip("自身名称")] private string name="enemy";     //敌人名字
-    [SerializeField] [Tooltip("自身能量")] private float energy=100f;    //自己的体力属性上限
-    [SerializeField] [Tooltip("自身追击时间")] private float hostility = 5f; 
+    [SerializeField] [Tooltip("自身名称")] private string name = "enemy";     //敌人名字
+    [SerializeField] [Tooltip("自身能量")] private float energy = 100f;    //自己的体力属性上限
+    [SerializeField] [Tooltip("自身追击时间")] private float hostility = 5f;
     [SerializeField] [Tooltip("能量恢复速度")] private float energy_increase = 35f;
     [Header("技能属性设置")]
     [SerializeField] [Tooltip("启用冲锋攻击")] private bool ishit;
@@ -21,7 +21,7 @@ public class AI : MonoBehaviour
 
     [SerializeField] [Tooltip("启用奔跑攻击")] private bool isrun; //启用奔跑
     [SerializeField] [Tooltip("启用奔跑攻击概率")] private int runrate;
-    [SerializeField] [Tooltip("奔跑时间")] private float runtime=3f;
+    [SerializeField] [Tooltip("奔跑时间")] private float runtime = 3f;
     [SerializeField] [Tooltip("奔跑速度")] private float runspeed = 6f;
     [SerializeField] [Tooltip("消耗能量")] private float runcost = 20;
 
@@ -32,15 +32,21 @@ public class AI : MonoBehaviour
     [Header("自身属性状态显示（不可修改）")]
     [SerializeField] [Tooltip("是否激活")] private bool ishost;
     [SerializeField] [Tooltip("是否追击")] private bool iskeephost;
-    [SerializeField] [Tooltip("是否游击")] private bool isattract;
+    [SerializeField] [Tooltip("是否游击")] private bool isattract=true;
     [SerializeField] [Tooltip("玩家距离")] private float distance;
     [SerializeField] [Tooltip("是否奔跑")] private bool isrunning;
     [SerializeField] [Tooltip("是否冲锋")] private bool ishitting;
     [SerializeField] [Tooltip("体力能量状态")] private float thisenergy;      //敌人当前体力属性
     [SerializeField] [Tooltip("追击能量状态")] private float thishostility;    //敌人当前追击属性
+    [Header("攻击属性")]
+    [SerializeField] [Tooltip("击飞速度")] private float hittedtime = 2f;
+    [SerializeField] [Tooltip("击飞速度")] private float hittedspend = 30f;
+    [SerializeField] [Tooltip("击飞高度")] private float hittedhigh = 1f;
     private float thisrunningtime;
     private float thishittingtime;
 
+    private Vector3 hitposition; //设置击飞方向
+    public float thishittedtime;
     private Transform target;   //设置追踪目标的位置
     private Transform myself;   //自己本体
     private Vector3 position;  //自己本体的初始位置
@@ -48,15 +54,20 @@ public class AI : MonoBehaviour
     
     public float MoveSpeed = 2.5f; //敌人移动速度
     private NavMeshAgent navMeshAgent;  //设置寻路组件
-   
-   
+    public CharacterController characterController;
+
+
+
+
+
 
 
     void Start()
     {
-        myself = gameObject.transform;
+        myself = GameObject.Find(name).transform;
         position = myself.transform.position;
-        target = GameObject.FindWithTag("Player").transform;  //获取游戏中主角的位置，在我的工程里面主角的标签是Player
+        target = GameObject.Find("Player").transform;  //获取游戏中主角的位置，在我的工程里面主角的标签是Player
+        characterController = GameObject.Find("Player").GetComponent<CharacterController>();
         navMeshAgent = GetComponent<NavMeshAgent>();
         navMeshAgent.speed = MoveSpeed;  //设置寻路器的行走速度
         if (navMeshAgent == null)
@@ -73,7 +84,7 @@ public class AI : MonoBehaviour
 
     void Update()
     {
-        //Debug.Log(position);
+        Debug.Log(position);
         distance = (target.transform.position - myself.transform.position).magnitude;
         if (distance < beginhost_distance && thishostility>=hostility) {
             ishost = true;
@@ -154,11 +165,21 @@ public class AI : MonoBehaviour
             {
                 ishitting = false;
                 navMeshAgent.speed = MoveSpeed;
+                if (distance < 5) {
+                    hitposition = target.transform.position - myself.transform.position;
+                    thishittedtime = hittedtime;
+                    hitposition = hitposition / distance;
+                    hitposition.y += hittedhigh;
+                }
             }
         }
         if (thisenergy < energy) {
             thisenergy += energy_increase * Time.deltaTime;
         }
+        if (thishittedtime > 0) {
+            thishittedtime -= Time.deltaTime;
 
+            characterController.Move(hitposition * hittedspend * Time.deltaTime*thishittedtime*thishittedtime/hittedtime/hittedtime);
+        }
     }
 }
